24e9634a54391a12b7b9d9b83caa4109575e1f6c
iyyamperumal <sathishkannan140@gmail.com>
2025-04-25 18:18:19 +0530

    updated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import recipeRoutes from "./routes/recipes.js";
-
+dotenv.config();
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,10 +16,7 @@ app.use("/api/recipes", recipeRoutes);
 app.use("/images", express.static("images"));
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("Connected to MongoDB Atlas");
   app.listen(process.env.PORT || 5000, () => {
